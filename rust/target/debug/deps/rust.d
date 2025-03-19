bc2dd486d4f4e65cb9d61caf03d33b3b89d1a732
Wallem-1089 <73552998+Wallem-1089@users.noreply.github.com>
2025-03-19 11:19:00 +0100

    Add files via upload

---
+++ rust/target/debug/deps/rust.d
@@ -0,0 +1,5 @@
+C:\Users\Walter\Documents\rust_cohort_one_assignment\rust\target\debug\deps\rust.exe: src\main.rs
+
+C:\Users\Walter\Documents\rust_cohort_one_assignment\rust\target\debug\deps\rust.d: src\main.rs
+
+src\main.rs:
---
+++ rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+C:\Users\Walter\Documents\rust_cohort_one_assignment\rust\target\debug\rust.exe: C:\Users\Walter\Documents\rust_cohort_one_assignment\rust\src\main.rs
